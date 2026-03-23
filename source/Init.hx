356d8256207e0e7b8941ccc072498c8b6245a4f4
DiogoTV <89536150+DiogoTVV@users.noreply.github.com>
2026-03-23 18:33:24 -0300

    this didnt work as well as i think it would

--- source/Init.hx
+++ source/Init.hx
@@ -30,10 +30,6 @@ class Init extends MusicBeatState
 		#if android
 		FlxG.android.preventDefaultKeys = [BACK];
 		#end
-		
-		#if mobile
-        FlxG.scaleMode = new flixel.system.scaleModes.FixedScaleAdjustSizeScaleMode();
-        #end
 	}
 
 	/*
