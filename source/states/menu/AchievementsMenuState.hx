90fe2dc0af3f030864226f187d007fed420225fe
valmarosaalves-cmyk <valmarosaalves@gmail.com>
2026-04-30 09:52:15 -0300

    Update AchievementsMenuState.hx

--- source/states/menu/AchievementsMenuState.hx
+++ source/states/menu/AchievementsMenuState.hx
@@ -1,91 +1,87 @@
-package states;
+package states.menu;
 
-import backend.Achievements;
-import backend.ClientPrefs;
-import objects.Alphabet;
-import objects.AttachedSprite; // Doido costuma usar AttachedSprite para ícones
 import flixel.FlxG;
 import flixel.FlxSprite;
-import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.group.FlxGroup;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
+import backend.Paths;
+import objects.menu.Alphabet;
 
 class AchievementsMenuState extends MusicBeatState
 {
-	var options:Array<String> = [];
+	var achievementList:Array<String> = ["friday_night_felon", "she_call_me_pico", "debugger"];
 	private var grpOptions:FlxTypedGroup<Alphabet>;
+	private var iconArray:Array<FlxSprite> = [];
 	private static var curSelected:Int = 0;
-	private var achievementArray:Array<AttachedSprite> = [];
-	private var descText:FlxText;
 
-	override function create()
-	{
-		#if DISCORD_ALLOWED
-		backend.DiscordClient.changePresence("Visualizando Conquistas", null);
-		#end
-
-		var menuBG:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuBGBlue'));
-		menuBG.antialiasing = ClientPrefs.data.antialiasing;
+	override function create() {
+		var menuBG:FlxSprite = new FlxSprite().loadGraphic(Paths.image('Achievements/menuBGBlue'));
+		menuBG.antialiasing = true;
+		menuBG.setGraphicSize(Std.int(menuBG.width * 1.1));
+		menuBG.updateHitbox();
+		menuBG.screenCenter();
 		add(menuBG);
 
 		grpOptions = new FlxTypedGroup<Alphabet>();
 		add(grpOptions);
 
-		// Na Doido Engine, certifique-se de que Achievements.load() ou similar foi chamado
-		Achievements.loadAchievements(); 
-		
-		for (i in 0...Achievements.achievementsStuff.length) {
-			var isUnlocked:Bool = Achievements.isAchievementUnlocked(Achievements.achievementsStuff[i][2]);
-			
-			var optionText:Alphabet = new Alphabet(0, (70 * i) + 30, Achievements.achievementsStuff[i][0], true);
-			optionText.isMenuItem = true;
-			optionText.targetY = i;
+		for (i in 0...achievementList.length) {
+			var optionText:Alphabet = new Alphabet(0, 0, achievementList[i].replace('_', ' '), true);
+			optionText.ID = i;
+			optionText.screenCenter(X);
+			optionText.y = (i * 120) + 200;
 			grpOptions.add(optionText);
 
-			// Ícone da conquista
-			var icon:AttachedSprite = new AttachedSprite('achievements/' + Achievements.achievementsStuff[i][2]);
-			icon.sprTracker = optionText;
-			icon.copyAlpha = true;
-			icon.alpha = isUnlocked ? 1 : 0.4; // Se não desbloqueou, fica transparente
-			achievementArray.push(icon);
+			var icon:FlxSprite = new FlxSprite();
+			icon.loadGraphic(Paths.image('Achievements/' + achievementList[i]));
+			icon.ID = i;
+			iconArray.push(icon);
 			add(icon);
 		}
-
-		descText = new FlxText(150, 600, 980, "", 32);
-		descText.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-		descText.scrollFactor.set();
-		add(descText);
-
+		
 		changeSelection();
 		super.create();
 	}
 
-	override function update(elapsed:Float)
-	{
-		if (controls.UI_UP_P) changeSelection(-1);
-		if (controls.UI_DOWN_P) changeSelection(1);
-
-		if (controls.BACK) {
-			FlxG.sound.play(Paths.sound('cancelMenu'));
-			MusicBeatState.switchState(new MainMenuState());
-		}
+	override function update(elapsed:Float) {
 		super.update(elapsed);
+
+		// Controles Universais (PC e Android via toque)
+		if (FlxG.keys.justPressed.UP) changeSelection(-1);
+		if (FlxG.keys.justPressed.DOWN) changeSelection(1);
+		
+		if (FlxG.keys.justPressed.ESCAPE || FlxG.android.justPressed.BACK) {
+			FlxG.sound.play(Paths.sound('cancelMenu'));
+			MusicBeatState.switchState(new states.menu.MainMenuState());
+		}
+
+		// Toque na tela para Android (Cima sobe, Baixo desce)
+		if (FlxG.touches.justStarted().length > 0) {
+			var touch = FlxG.touches.justStarted()[0];
+			if (touch.y < FlxG.height / 2) changeSelection(-1);
+			else changeSelection(1);
+		}
+
+		for (i in 0...grpOptions.members.length) {
+			var item = grpOptions.members[i];
+			item.screenCenter(X);
+			item.y = FlxG.math.FlxMath.lerp(item.y, (FlxG.height / 2) + ((i - curSelected) * 140), 0.15);
+			
+			if (iconArray[i] != null) {
+				iconArray[i].y = item.y;
+				iconArray[i].x = item.x - 110;
+			}
+		}
 	}
 
-	function changeSelection(change:Int = 0)
-	{
-		curSelected += change;
-		if (curSelected < 0) curSelected = Achievements.achievementsStuff.length - 1;
-		if (curSelected >= Achievements.achievementsStuff.length) curSelected = 0;
-
-		var bullShit:Int = 0;
-		for (item in grpOptions.members) {
-			item.targetY = bullShit - curSelected;
-			bullShit++;
-			item.alpha = (item.targetY == 0) ? 1 : 0.6;
+	function changeSelection(change:Int = 0) {
+		curSelected = FlxG.math.FlxMath.wrap(curSelected + change, 0, achievementList.length - 1);
+		if(change != 0) FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
+		
+		for (i in 0...grpOptions.members.length) {
+			grpOptions.members[i].alpha = (i == curSelected) ? 1 : 0.6;
+			if(iconArray[i] != null) iconArray[i].alpha = (i == curSelected) ? 1 : 0.6;
 		}
-
-		descText.text = Achievements.achievementsStuff[curSelected][1]; // Descrição do JSON
-		FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
 	}
 }
