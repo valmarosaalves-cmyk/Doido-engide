ec85ceceeb744938da74e57811183d971e79a9ef
valmarosaalves-cmyk <valmarosaalves@gmail.com>
2026-04-30 09:53:05 -0300

    Update PauseSubState.hx

--- source/subStates/PauseSubState.hx
+++ source/subStates/PauseSubState.hx
@@ -1,125 +1,86 @@
 package subStates;
 
-import backend.song.Conductor;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.group.FlxGroup;
-import flixel.math.FlxMath;
-import flixel.math.FlxRect;
 import flixel.text.FlxText;
-import flixel.tweens.FlxEase;
-import flixel.tweens.FlxTween;
-import flixel.util.FlxTimer;
-import flixel.sound.FlxSound;
-import objects.menu.AlphabetMenu;
-import states.*;
+import flixel.util.FlxColor;
+import backend.Paths;
+import objects.menu.Alphabet;
+import states.PlayState;
 
 class PauseSubState extends MusicBeatSubState
 {
-	var optionShit:Array<String> = ["resume", "restart song", "botplay", "options", "exit to menu"];
+	var grpMenuShit:FlxTypedGroup<Alphabet>;
+	var menuItems:Array<String> = ['Resume', 'Restart Song', 'Exit to Menu'];
 	var curSelected:Int = 0;
-	
-	var optionsGrp:FlxTypedGroup<AlphabetMenu>;
-	var menuBox:FlxSprite;
-	var nameBox:FlxSprite;
-	var songNameTxt:FlxText;
-	var mebg:FlxSprite;
-	
-	var pauseSong:FlxSound;
-	var playstate:PlayState;
 
 	public function new()
 	{
 		super();
-		playstate = PlayState.instance;
-		this.cameras = [FlxG.cameras.list[FlxG.cameras.list.length - 1]];
-
-		// 1. Fundo MEBG (atrás de tudo)
-		mebg = new FlxSprite().loadGraphic(Paths.image('mebg'));
-		mebg.antialiasing = true;
-		mebg.scale.set(1.4, 1.4);
-		add(mebg);
-
-		// Movimento escada infinito
-		FlxTween.tween(mebg, {x: mebg.x + 150, y: mebg.y - 150}, 2, {type: LOOPING, ease: FlxEase.linear});
-
-		// 2. Blocos de UI
-		menuBox = new FlxSprite().makeGraphic(420, 550, 0xAA000000);
-		menuBox.screenCenter();
-		add(menuBox);
-
-		nameBox = new FlxSprite(menuBox.x + 35, menuBox.y + 30).makeGraphic(350, 60, 0xFF000000);
-		add(nameBox);
-
-		// 3. Nome da Música (Letreiro Rolante)
-		var displaySong = PlayState.SONG.song.toUpperCase();
-		songNameTxt = new FlxText(0, nameBox.y + 12, 0, displaySong);
-		songNameTxt.setFormat("assets/fonts/pixel-game.regular.otf", 32, 0xFFFFFFFF, LEFT);
-		add(songNameTxt);
-
-		// Máscara para o texto não vazar do nameBox
-		songNameTxt.clipRect = new FlxRect(0, 0, nameBox.width, nameBox.height);
 		
-		// Inicia fora à direita e corre para a esquerda
-		songNameTxt.x = nameBox.x + nameBox.width;
-		FlxTween.tween(songNameTxt, {x: nameBox.x - songNameTxt.width}, 5, {type: LOOPING});
+		// Fundo escuro para destacar o menu
+		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		bg.alpha = 0.6;
+		add(bg);
 
-		// 4. Opções do Menu
-		optionsGrp = new FlxTypedGroup<AlphabetMenu>();
-		add(optionsGrp);
+		grpMenuShit = new FlxTypedGroup<Alphabet>();
+		add(grpMenuShit);
 
-		for(i in 0...optionShit.length)
+		for (i in 0...menuItems.length)
 		{
-			var newItem = new AlphabetMenu(0, 0, optionShit[i], true);
-			newItem.ID = i;
-			newItem.scale.set(0.55, 0.55); // Pequeno para caber
-			newItem.updateHitbox();
-			newItem.y = menuBox.y + 130 + (i * 75);
-			newItem.screenCenter(X);
-			optionsGrp.add(newItem);
+			var item:Alphabet = new Alphabet(0, 0, menuItems[i], true);
+			item.ID = i;
+			item.screenCenter(X);
+			item.y = (i * 120) + 250;
+			grpMenuShit.add(item);
 		}
 
-		// IMPORTANTE: Suporte para Mobile (Não apagar)
-		#if TOUCH_CONTROLS
-		#end
-
 		changeSelection();
+		
+		// Importante: pausa o som do jogo ao entrar
+		if (FlxG.sound.music != null) FlxG.sound.music.pause();
 	}
 
 	override function update(elapsed:Float)
 	{
 		super.update(elapsed);
 
-		// Atualiza a máscara do texto enquanto ele anda
-		if (songNameTxt.clipRect != null) {
-			songNameTxt.clipRect.x = nameBox.x - songNameTxt.x;
-			songNameTxt.clipRect = songNameTxt.clipRect;
-		}
+		if (FlxG.keys.justPressed.UP) changeSelection(-1);
+		if (FlxG.keys.justPressed.DOWN) changeSelection(1);
 
-		if(Controls.justPressed(UI_UP)) changeSelection(-1);
-		if(Controls.justPressed(UI_DOWN)) changeSelection(1);
-		if(Controls.justPressed(BACK)) close();
-
-		if(Controls.justPressed(ACCEPT))
+		if (FlxG.keys.justPressed.ENTER || FlxG.android.justPressed.BACK || (FlxG.touches.justStarted().length > 0 && FlxG.touches.justStarted()[0].justPressed))
 		{
-			switch(optionShit[curSelected])
+			var daSelected:String = menuItems[curSelected].toLowerCase();
+
+			switch (daSelected)
 			{
-				case "resume": close();
-				case "restart song": Main.resetState();
-				case "exit to menu": PlayState.sendToMenu();
+				case "resume":
+					close();
+				case "restart song":
+					MusicBeatState.resetState();
+				case "exit to menu":
+					PlayState.seenCutscene = false;
+					PlayState.deathCounter = 0;
+					MusicBeatState.switchState(new states.menu.MainMenuState());
 			}
 		}
 	}
 
-	function changeSelection(change:Int = 0)
-	{
-		curSelected = FlxMath.wrap(curSelected + change, 0, optionShit.length - 1);
-		if(change != 0) FlxG.sound.play(Paths.sound("menu/scrollMenu"));
+	// ESSA FUNÇÃO RESOLVE O CONGELAMENTO
+	override function destroy() {
+		if (FlxG.sound.music != null) FlxG.sound.music.resume(); // Volta a música
+		super.destroy();
+	}
 
-		for(item in optionsGrp)
-		{
-			item.alpha = 0.5;
-			if(item.ID == curSelected) item.alpha = 1;
+	function changeSelection(change:Int = 0):Void
+	{
+		curSelected = FlxG.math.FlxMath.wrap(curSelected + change, 0, menuItems.length - 1);
+		if(change != 0) FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
+
+		for (item in grpMenuShit.members) {
+			item.alpha = 0.6;
+			if (item.ID == curSelected) item.alpha = 1;
 		}
 	}
 			}
